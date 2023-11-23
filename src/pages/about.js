08bb4594dadb237035e3bd770d9893c82e1d8688
summercody <summercody@gmail.com>
2023-11-23 13:44:15 -0500

    fixing typos, small aesthetic changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -81,7 +81,7 @@ export default function About() {
             style={{
               display: 'flex',
               gap: '30px',
-              width: '820px',
+              width: '800px',
               paddingBottom: '80px'
             }}>
               <motion.div variants={prof} style={{ minWidth: '320px'}}>
@@ -90,36 +90,39 @@ export default function About() {
                 }} />
               </motion.div>
               <motion.div variants={textRight} style={{ minWidth: '320px'}}>
-              <div className="aboutContent" style={{ lineHeight: '26px' }}>
-                <p>
+              <div className="aboutContent" style={{ lineHeight: '27px' }}>
+                <p style={{ fontSize: '15px'}}>
                   I'm a self-taught software developer with experience across the stack. Having graduated from Dartmouth College with a degree in Music Composition,
                   I am interested in both the technical nature of programming and the ways in which it demands creativity and innovation.
                 </p>
                 <br />
-                <p>
+                <p style={{ fontSize: '15px'}}>
                   After graduating and spending time learning on my own, I worked for over a year at a small healthcare technology startup, where I honed my skills
-                  in frontend development, building and maintaining API's, database management, and more.
+                  in frontend web development, building and maintaining API's, database management, and more.
                 </p>
                 <br />
-                <p>
-                  Today, I work as a backend engineer at an international EdTech company, helping to improve the experience of teachers and students who interract
+                <p style={{ fontSize: '15px'}}>
+                  Today, I work as a backend engineer at an international ed-tech company, helping to improve the experience of teachers and students who interact
                   with our educational web platform.
                 </p>
                 <br />
-                <p>
+                <p style={{ fontSize: '15px'}}>
                   (I also love to travel! See this wonderful Icelandic horse mistaking my jacket for lunch.)
                 </p>
 
-                <div style={{ height: '2px', backgroundColor: '#7e913a', width: '80px', margin: '18px 0px' }}></div>
-                
+                <div style={{ height: '2px', backgroundColor: '#7e913a', width: '81px', margin: '18px 0px' }}></div>
+
                 <p style={{ fontSize: '15px' }}>
-                  <strong>Languages</strong>: Python &#8226; JS &#8226; CSS &#8226; SQL
+                  <strong>Languages</strong>: Python &#8226; JavaScript &#8226; CSS &#8226; SQL
                 </p>
                 <p style={{ fontSize: '15px', whiteSpace: 'nowrap' }}>
                   <strong>Libraries & Frameworks</strong>: Pytest &#8226; React &#8226; Pandas &#8226; SQLAlchemy &#8226; Flask
                 </p>
                 <p style={{ fontSize: '15px' }}>
                   <strong>Databases & Security</strong>: PostreSQL &#8226; AWS
+                </p>
+                <p style={{ fontSize: '15px' }}>
+                  <strong>Miscellaneous</strong>: Elasticsearch &#8226; Docker &#8226; Jenkins &#8226; Git &#8226; GitHub
                 </p>
             </div>
             </motion.div>
